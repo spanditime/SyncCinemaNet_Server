e8f99afed906e24b5dcabc68cf709471c24bacaa
smolyar4 <smolyar4@gmail.com>
2019-01-23 13:24:28 +0300

    Add function tcp_server()

--- video/demo.c
+++ video/demo.c
@@ -1,6 +1,10 @@
 #include "interfaces/tcp.h"
 #include <stdio.h>
+#include <sys/epoll.h>
+
+struct epoll_event main_event;
 
 int main() {
-  printf("%d\n",tcp_create(LOCAL, 8000, 1));
+  //printf("%d\n",tcp_create(LOCAL, 8000, 1));
+  tcp_server(&main_event,LOCAL, 8000);
 }
--- video/interfaces/tcp.h
+++ video/interfaces/tcp.h
@@ -1,5 +1,11 @@
 #ifndef SERVER_TCP
   #define SERVER_TCP 1
+  #define LISTEN 10000
+  #ifndef NULL
+    #define NULL (void*)0
+  #endif
+  #include <sys/epoll.h>
   enum {LOCAL, INET};
   int tcp_create(int net, int port, int size);
+  int tcp_server(struct epoll_event *event, int net, int port);
 #endif
