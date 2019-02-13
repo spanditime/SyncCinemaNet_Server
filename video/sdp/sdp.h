0d7f7e5cef806d2c9032e0960bae496a0d1baf7d
Eugene Frolov <spanditime@gmail.com>
2019-02-13 21:35:20 +0700

    add SdpContent struct

--- video/sdp/sdp.h
+++ video/sdp/sdp.h
@@ -5,7 +5,7 @@
     Look at SDP specification
     https://tools.ietf.org/html/rfc4566#page-7
   */
-  typedef struct SDP_{
+  typedef struct {
     int version;
     union{
       char *username;
@@ -20,6 +20,35 @@
       char *info;
     }session;
     char *URI;
+    struct SdpConnectionInfo{
+
+    } *connection_info;
+    struct SdpBandwidth{
+      
+    } *bandwidths;
+    int bandwithsCount;
+    long long int start_time,stop_time;
+    union SdpEncryption{
+      char *method;
+      char *key;
+    }encryption;
+    struct SdpAttribute{
+      char *attribute;
+      char *value;
+    } attributes;
+    struct SdpMedia{
+      char *type;
+      char *port;
+      char *proto;
+      char *format;
+      char *title;
+      char *connection_info;
+      struct SdpBandwidth *bandwidths;
+      int bandwidthsCount;
+      union SdpEncryption encryption;
+      struct SdpConnectionInfo connection_info;
+    } *mediums;
+    int mediumsCount;
   } SdpContent;
 
   int parse_sdp(SdpContent *sdp,char *str);
