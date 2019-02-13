6186371d755946c1815238ecf0a08b9fdb9ae697
Eugene Frolov <spanditime@gmail.com>
2019-02-13 19:09:08 +0700

    add basic SDP parser structure

---
+++ video/sdp/sdp.c
@@ -0,0 +1,21 @@
+#include <string.h>
+
+#include "sdp.h"
+#include "interfaces/errors.h"
+
+
+int parse_sdp(SdpContent *sdp,char *str){
+  die_err(SE_DEV);
+  return -1;
+  /*
+    parser
+  */
+}
+
+int sdpstr(char *str,SdpContent *sdp){
+  die_err(SE_DEV);
+  return -1;
+  /*
+    reparser
+  */  
+}
